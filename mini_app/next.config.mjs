d02e88801361a901ff0fbe98cb87d42362c6f63c
NeewMeta88 <egor.rodionovv@gmail.com>
2025-09-30 22:26:40 +0300

    fix NextJs

--- mini_app/next.config.mjs
+++ mini_app/next.config.mjs
@@ -1,22 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    serverActions: {
-      bodySizeLimit: "2mb",
-    },
-  },
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          //  CSP:
-          // { key: "Content-Security-Policy", value: "default-src 'self'; script-src 'self' https://telegram.org; frame-src https://oauth.telegram.org" },
-        ],
-      },
-    ];
-  },
+  experimental: { serverActions: { bodySizeLimit: "2mb" } },
 };
 
 export default nextConfig;
