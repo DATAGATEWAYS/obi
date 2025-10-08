60aed6120f266f58dca122ce2e078351fd22b640
NeewMeta88 <egor.rodionovv@gmail.com>
2025-10-08 21:42:37 +0300

    prerender fix

--- mini_app/app/dashboard/page.tsx
+++ mini_app/app/dashboard/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+export const dynamic = "force-dynamic";
 import {useEffect, useState} from "react";
 import {usePrivy} from "@privy-io/react-auth";
 import {useRouter} from "next/navigation";
